74dc2f41a1a0a917ab685d33425fd15fbc7b8528
colton chilelli <colton.chilelli@student.centralia.edu>
2020-06-05 10:30:15 -0700

    fixed spelling

--- Models/album/AlbumRepo.cs
+++ Models/album/AlbumRepo.cs
@@ -16,11 +16,11 @@ namespace MediaMattersCCV2.Models.album
         public IEnumerable<Album> getAlbums() =>
             new List<Album>
             {
-                new Album{aTitle = "Mostly We Grow Pt. 1", image="~/images/Albums/MWGPt1.jpg", artist="Half an Oranage", notes="I like it simply it's fun and catchy"},
-                new Album{aTitle = "Thrill of it", artist="Conro", image="~/images/Albums/ThrillOfIt.jpg", notes="I don't love the album, but I like the artists and this album is decent" },
+                new Album{aTitle = "Mostly We Grow Pt. 1", image="~/images/Albums/MWGPt1.jpg", artist="Half an Orange", notes="I like it simply it's fun and catchy"},
+                new Album{aTitle = "Thrill of it", artist="Conro", image="~/images/Albums/ThrillOfIt.jpg", notes="I don't love the album, but I like the artist and this album is decent" },
                 new Album{aTitle="Monstercat Instinct Vol. 4", artist="Monstercat(Various)", image="~/images/Albums/MCIV4.jpg", notes="Less an album and more a compilation of music, but most of it's not bad" },
                 new Album{aTitle="PLAY (The Remixes)", artist="Multiple", image="~/images/Albums/Play(Remixes).jpg", notes="More of an EP than an album, it's just all of the remixes of a single song by Tokyo Machine" },
-                new Album{aTitle="Nerds by Nature", artist="Pegboard Nerds", image="~/images/Albums/NBN.jpeg", notes="Used to be one of my more liked artists, not so much anymore, but some of their older stuff is decent" }
+                new Album{aTitle="Nerds by Nature", artist="Pegboard Nerds", image="~/images/Albums/NBN.jpeg", notes="Used to be one of my more liked artist, not so much anymore, but some of their older stuff is pretty good" }
             };
     }
 }
